7bdfe9b37a441d88406aa563e71084e485a1654f
Xilin Chen <xilinc@med.umich.edu>
2022-06-22 13:50:17 -0400

    remove multiple procedures based on ECV

--- R/add_n_proc_admisssion.R
+++ R/add_n_proc_admisssion.R
@@ -42,3 +42,42 @@ remove_multi_proc <- function(data) {
   }
   
 }
+
+
+#' Remove multiple ECV procedures and keep one unique ECV case
+#'
+#' @param data  medicare data
+#'
+#' @return
+#' @export
+#'
+
+remove_multi_ecv_proc <- function(data) {
+  model_vars = c(
+    "flg_cmp_po_severe",
+    # outcome
+    "val_yr_practice",
+    # primary variable
+    "flg_male",
+    "age_at_admit",
+    "e_race_wbho",
+    "e_admit_type",
+    "AHRQ_score",
+    "e_ses_5grp",
+    "facility_clm_yr",
+    "had_assist_surg",
+    "id_physician_npi",
+    "facility_prvnumgrp",
+    "e_proc_grp_lbl"
+  )
+  
+  # remove multiple procedures using ECS
+  data[, n_ecs_proc_admission := uniqueN(e_proc_grp_lbl), by = .(member_id, dt_profsvc_start, dt_profsvc_end)]
+  
+  medicare_unique = data %>%
+    filter(n_ecs_proc_admission ==1)  %>%
+    distinct(!!!syms(model_vars),.keep_all = TRUE)
+  
+}
+
+
